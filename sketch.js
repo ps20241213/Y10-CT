5161868694dcdd72f98b847bdd5cd8e8172c5847
ps20241213 <46880387+ps20241213@users.noreply.github.com>
2019-01-21 12:32:05 +0800

    tutorial 1.1
    tutorial 1.1

--- sketch.js
+++ sketch.js
@@ -1,19 +1,22 @@
 function setup()
 {
   //create a drawing service 700px wide, 500px tall
-  createCanvas(1170,700);
+  createCanvas(1200,1000);
 
 }
 
 function draw()
 {
   //create a background using RGB values
-  background(205,140,210)
-
+  background(158,114,1290)
   // change the paint brush to a specific color according to RGB values
-  fill(180,190,125);
-
+  fill(1119,190,125);
   // create a rectangle at x,y coordinates 10,10 with length x height at 100 x 50
-  rect(130,150,100,500);
+  rect(1099,499,100,500);
+  rect(1189,1,10,50);
+  rect(480,250,120,250);
+  rect(1,729,100,270);
+  rect(1,1,200,150);
+  rect(699,100,300,320);
 
 }
