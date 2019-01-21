0fcda30a92534142c722b086fa8c7a885294d717
ps20241213 <46880387+ps20241213@users.noreply.github.com>
2019-01-21 12:08:16 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup()
+{
+  //create a drawing service 700px wide, 500px tall
+  createCanvas(1170,700);
+
+}
+
+function draw()
+{
+  //create a background using RGB values
+  background(205,140,210)
+
+  // change the paint brush to a specific color according to RGB values
+  fill(180,190,125);
+
+  // create a rectangle at x,y coordinates 10,10 with length x height at 100 x 50
+  rect(130,150,100,500);
+
+}
