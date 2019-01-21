803e37a07e6642eef935b9887179dff19f577b02
ps20241213 <46880387+ps20241213@users.noreply.github.com>
2019-01-21 12:57:18 +0800

    tutorial 1.1 (complete, v2)
    check history for changes

--- sketch.js
+++ sketch.js
@@ -4,7 +4,9 @@ function setup()
   createCanvas(1200,1000);
 
 }
-//did you know that it is possible to pasteurize a glass of milk in the Dasht-E-Lut desert in Iran simply by leaving it in the open air?
+//did you know that it is possible to pasteurize a glass of milk in the
+//Dasht-E-Lut desert in Iran simply by leaving it in the open air?
+
 function draw()
 {
   //create a background using RGB values
@@ -12,11 +14,13 @@ function draw()
   // change the paint brush to a specific color according to RGB values
   fill(111,190,25);
   // create a rectangle at x,y coordinates 10,10 with length x height at 100 x 50
+  //reference points for rectangles is at TOP LEFT CORNER; account for shifts
   rect(1099,499,100,500);
   rect(1189,1,10,50);
-  rect(480,250,120,250);
+  rect(48,250,120,250);
   rect(1,729,100,270);
   rect(1,1,200,150);
   rect(699,100,300,320);
+  rect(590,490,10,10);
 
 }
